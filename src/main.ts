2eedc1e71651f4e48c91389b2d8d35a2516d1324
rorn <rysarorn8@gmail.com>
2022-03-01 18:23:59 +0700

    jj

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { keys } from './Keys/keys';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.PORT || 5000);
   // await app.listen(configService.get('PORT') || 5000);
 }
 
