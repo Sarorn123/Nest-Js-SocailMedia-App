986a697f6d611a406df554fcbd498c86b6b28155
Sarorn123 <87157752+Sarorn123@users.noreply.github.com>
2022-03-01 09:13:31 +0700

    gg

--- project_key.ts
+++
@@ -1,6 +0,0 @@
-export const ProjectKey = {
-  DB_URI:
-    'mongodb+srv://rysarorn1111:rysarorn1111@cluster0.bvz1i.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-  SECRET_KEY: 'rorn_socail_media_project_key',
-  PORT: 3000,
-};
